c32c8d6c7cea9d87b3df395c92339f0d0603248f
Viktoria Bayer <bayer.skaya@gmail.com>
2023-01-15 12:09:13 +0100

    bug-fix2

--- src/components/TestProfileList/TestProfileList.js
+++ src/components/TestProfileList/TestProfileList.js
@@ -1,19 +1,19 @@
-// import { TestProfile } from "../TestProfile/TestProfile";
-import PropTypes from "prop-types";
-// import {TestList} from './TestProfileList.styled'
+// // import { TestProfile } from "../TestProfile/TestProfile";
+// import PropTypes from "prop-types";
+// // import {TestList} from './TestProfileList.styled'
 
-export const TestProfileList = ({content})=>{
-    return (
-        <TestList>
-      {content.map(user =>(
-        <li key={user.id}>
-            <TestProfile data={user}/>
-        </li>
-         ))}
-        </TestList>
-            );
-};
+// export const TestProfileList = ({content})=>{
+//     return (
+//         <TestList>
+//       {content.map(user =>(
+//         <li key={user.id}>
+//             <TestProfile data={user}/>
+//         </li>
+//          ))}
+//         </TestList>
+//             );
+// };
 
-TestProfileList.propTypes = {
-content: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.number.isRequired}))
-}
+// TestProfileList.propTypes = {
+// content: PropTypes.arrayOf(PropTypes.shape({id: PropTypes.number.isRequired}))
+// }
--- src/components/TestProfileList/TestProfileList.styled.js
+++ src/components/TestProfileList/TestProfileList.styled.js
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-export const TestList = styled.ul`
-display: flex;
-gap: 16px;
-list-style: none;
-margin:0;
-padding:0;
-`
+// export const TestList = styled.ul`
+// display: flex;
+// gap: 16px;
+// list-style: none;
+// margin:0;
+// padding:0;
+// `
